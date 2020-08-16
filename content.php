3532cda0a25fa7d6097d5a1c378464e375599c8f
cinicDiver <oscarframirezp@hotmail.com>
2020-08-16 01:08:26 -0500

    Details

--- content.php
+++ content.php
@@ -7,7 +7,7 @@
         <link rel="stylesheet" href="../styling/minDecor.css" type="text/css"/>
     </head>
     <body>
-        <h1 class="title1">Bitácora</h>
+        <h1 class="title1">Contenido</h>
         <div></div>
         <h2>Actividades:</h2>
         <ul>
@@ -15,12 +15,12 @@
                 <li><a href="./lastDance/Ep1.php">Episode 1</a></li>
                 </ul></li>
         </ul>
-        <div style="position:fixed;border:3px solid black;padding:5px;bottom:0;right:0;">
-            <p><a href="./index.php">Volver al inicio.</a></p>
-        </div>
         <div style="background-color:#E8B631;position:fixed;bottom:90;width:100%;height:30px"><p></p></div>
         <div style="background-color:#E59D1B;position:fixed;bottom:60;width:100%;height:30px"><p></p></div>
         <div style="background-color:#E08307;position:fixed;bottom:30;width:100%;height:30px"><p></p></div>
         <div style="background-color:#DB6700;position:fixed;bottom:0;width:100%;height:30px"><p></p></div>
+        <div style="position:fixed;padding:5px;bottom:0;right:0;">
+            <p><a href="./index.php" style="text-decoration:none;font-family:Arial Black,sans-serif">Volver al inicio.</a></p>
+        </div>
     </body>
 </html>
