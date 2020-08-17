849d7c5f58d1a15579251441f4f65af6e9c0a4be
cinicDiver <oscarframirezp@hotmail.com>
2020-08-16 21:30:42 -0500

    Episode 2 entry finished.

--- lastDance/Ep2.php
+++ lastDance/Ep2.php
@@ -13,13 +13,73 @@
         <nav class="navb">
             <ul class="navl">
                 <li class="navli"><a class="nava" href="./EpL.php">Episodios</a></li>
-                <li class="navli"><a class="nava" href="./Ep3.php">Siguiente</a></li>
                 <li class="navli"><a class="nava" href="./Ep1.php">Anterior</a></li>
+                <li class="navli"><a class="nava" href="./Ep3.php">Siguiente</a></li>
                 <li class="navli"><a class="nava" href="../content.php">Contenido</a></li>
             </ul>
         </nav>
         <h1 class="title1">Episode 2</h1>
         <div></div>
+        <div class="quoteBox">
+            <p class="quote"><b>"Whenever they speak Michael Jordan they should speak Scottie Pippen"</b></p>
+            <p class="quoteA"><b>-Michael Jordan, 2019-</b></p>
+        </div>
+        <div class="par">
+            <p>Pippen fue uno de los grandes del equipo de los 90's, un jugador "vesatil", "atletico", "letal". Es claro que Pippen representaba una herramienta 
+                para los Bulls, y las derrotas que se dieron durante la recuperación de su lesión en el '97 muestran cómo se configuraba un equipo al rededor de un 
+                estilo de juego que involucraba al grupo entero. Sin embargo, esos promeros partidos demuestran igualmente una falencia común y peligrosa de los equipos; 
+                los esquemas centrados en particulares.</p>
+            
+            <p>Por otro lado, Jordan señala la importancia del acompañamiento de Pippen a su nivel personal, resalta que las victorias no las consigue únicamente él, 
+                sino tambien Scottie. El mejor jugador del mundo tenía su #2 para acompañarlo en el camino. Me parece fundamental, podré ser el mejor jugador en la cancha, 
+                pero no puedo lanzar y atrapar el disco inmediatamente. </p>
+        </div>
+        <div class="quoteBox">
+            <p class="quote"><b>"Pippen is as underpaid as underpaid gets"</b></p>
+            <p class="quoteA"><b>-Reporter, 1997-</b></p>
+        </div>
+        <div class="par">
+            <p>Creo que es innegable que la plata mueve el mundo. No es una visión reconfortante, pero es inevitable eliminar el valor <span style="text-decoration:underline">adquirido</span> 
+                del dinero, más aún en instancias profesionales, donde se relaciona directamente con la vida personal. Creo que este hecho se convierte en un 
+                obstáculo y que, incluso en un escenario de profesionales que jugaban Basket también por otros motivos, puede generar conflictos de ego y deseo 
+                perjudicanto los procesos.</p>
+
+            <p>El dinero juega un rol difernte en mi Ultimate, puesto que, aunque dedique gran parte de mi tiempo, no es mi fin último sostenerme con él, sin emabrgo, 
+                entra en otra categoría de conflicto, donde se debe de priorizar respecto al diario vivir, y la decisión de invertir o no en el Ultimate puede chocar 
+                con la recompensa de hacerlo, y por lo tanto, con la noción de valor personal que se pueda construir, rozando la linea del ego delicadamente.</p>
+
+            <p>Creo que, en esta posición cómoda, saber priorizar la satisfacción intrínseca del deporte sobre la posibilidad si quiera de recompensa económica es vital 
+                para conservar la pureza del deporte.</p>
+        </div>
+        <div class="quoteBox">
+            <p class="quote"><b>"My innate personality is to win at all costs. If I have to do it myself, I'm gonna do it"</b></p>
+            <p class="quoteA"><b>-Michael Jordan, 2019-</b></p>
+        </div>
+        <div class="par">
+            <p>Jordan siendo Jordan, una fuerza imparable. Creo que las actitudes de Jordan rayan fuertemente en el egocentrismo, lo cual considero peligroso y lo 
+                he experimentado de primera mando, sin embargo, esa delgada linea entre la determinación y la obstinación tiene sus recompensas, y Jordan las muestra 
+                con su proceso en los Bulls.</p>
+            
+            <p>El espectáculo que montó en el Boston Garden es una buena primera evidencia de ese instinto depredador e imponente que caracteriza su carrera 
+                y que se convierte en un ejemplo en lo personal de cómo aproximarse a la competencia. La sed de victoria en un jugador profesional debe de convertirse 
+                en combustible constante del motor deportivo, y la victoria puede generarse cuando el aporte de combustible se da en todo el equipo.</p>
+        </div>
+        <div class="quoteBox">
+            <p class="quote"><b>"It depends how fucking bad the headache is."</b></p>
+            <p class="quoteA"><b>-Michael Jordan, 1986-</b></p>
+        </div>
+        <div class="par">
+            <p>Durante su lesión en la temproada '85-'86, la lesión de Jordan amenazaba fuertemente su carrera y las oportunidades de la franquicia de acercarse al
+                título. Sin embargo, la amenza más fuerte fue a la personalidad de Jordan. Estar lesionado le impedía seguir con su impetú de ser el mejor y alcanzar la 
+                gloria, llevándolo incluso a arriesgarse a empeorar su lesión y terminar su carrera.</p>
+
+            <p>Siento que la determinación de Jordan parece ser inquebrantable y se encuentra ahí la base de el momentum con el cual llegó a ser el mejor
+                deportista del mundo, sin embargo, considero que Reissdorf tenía razón al señalar la relación existente entre el riesgo y la recompensa, 
+                y que, la sed de victoria no debe de cegarnos como jugadores a la hora de tomar decisiones difíciles.</p>
+
+            <p>Creo que a pesar de lo "escuelero" de la medida, las acciones de los Bulls como institución de frenar el impulso de Jordan sin embargo puede ser 
+                visto como una lección de paciencia y estrategia a la hora de incorparar decisiones laborales, familiares, personales, etc. con el desarrollo de nuestro deporte.</p>
+        </div>
         <div style="height:120px"></div>
         <div style="background-color:#E8B631;position:fixed;bottom:90;left:0;width:100%;height:30px"><p></p></div>
         <div style="background-color:#E59D1B;position:fixed;bottom:60;left:0;width:100%;height:30px"><p></p></div>
