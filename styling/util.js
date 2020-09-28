ac48eb89a6156ceb9ceff54b3bcd8171764542e3
cinicDiver <oscarframirezp@hotmail.com>
2020-09-27 20:10:39 -0500

    Added JavaScript for HTML entity generation.

---
+++ styling/util.js
@@ -0,0 +1,30 @@
+/*
+USING TABLE GENERATION:
+let element = document.getElementById("elementId")
+let headers = Object.keys(JSON_obj[0]);
+generateTableHead(element, headers);
+generateTable(element, JSON_obj);
+*/
+
+function generateTableHead(table, data) {
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for (let key of data) {
+        let th = document.createElement("th");
+        let text = document.createTextNode(key);
+        th.appendChild(text);
+        row.appendChild(th);
+    }
+}
+
+function generateTable(table, data) {
+    for (let element of data) {
+      let row = table.insertRow();
+      for (key in element) {
+        let cell = row.insertCell();
+        let text = document.createTextNode(element[key]);
+        cell.appendChild(text);
+        }
+    }
+}
+
